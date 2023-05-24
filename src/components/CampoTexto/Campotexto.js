8be34eacc68a11fbeb4b12b77c3589de9e082b23
Ham <ham@gmail.com>
2023-05-24 00:50:00 -0600

    required en CampoTexo

--- src/components/CampoTexto/Campotexto.js
+++ src/components/CampoTexto/Campotexto.js
@@ -3,7 +3,7 @@ const CampoTexto = (props) => { //Propiedades dde los componentes
     const placeholderModificado = `${props.placeholder}...`;
     return <div className= "Campo-Texto">
         <label>{props.titulo}</label>
-        <input placeholder= {placeholderModificado} />
+        <input placeholder= {placeholderModificado} required={props.required} />
     </div>
 }
 
