7cfd88f4f99ed447d0575cd5e99f9e5daaf41b70
Hamilton Durán <hamy.duran@gmail.com>
2023-05-27 17:45:39 -0600

    useState en botn add#

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
+import { useState } from 'react';
 import './App.css';
 import Header from './components/Header/Header.js';
 import Formulario from './components/Formulario/Formulario.js';
 import MiOrg from './components/MiOrg/MiOrg.js';
 function App() {
+
+  const [mostrarFormulario,actualizarFormulario] = useState(true)
+
+  //Ternario --> condicion ? seMuestra : noSeMuestra 
+  //{mostrarFormulario && <Formulario/>}
+
+  const cambiarMostrar = () => {
+    actualizarFormulario(!mostrarFormulario)
+  }
+
   return (
     <div>
       <Header/>
-      <Formulario/>
-      <MiOrg/>
+      {mostrarFormulario === true ? <Formulario/> : <div></div>}
+      
+      <MiOrg cambiarMostrar={cambiarMostrar}/>
     </div>
   );
 }
--- src/components/MiOrg/MiOrg.js
+++ src/components/MiOrg/MiOrg.js
@@ -1,8 +1,21 @@
 import "./MiOrg.css";
-const MiOrg = () => {
+
+const MiOrg = (props) => {
+    //Estado - hooks
+    //useState
+    //useState()
+    //const [nombreVar,funcionActualiza] = useState (valorInicial)
+    //const [mostrar,actualizarMostrar] = useState(true);
+
+    //const manejarClick = () =>{
+
+     //actualizarMostrar("Hola");
+
+   // }
+
     return <section className = "orgSection">
             <h3 className = "title">Mi Organización</h3>
-            <img src="/img/d.png" alt="add"/>
+            <img src="/img/d.png" alt="add" onClick = {props.cambiarMostrar}/>
     </section>
 
 }
